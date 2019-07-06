8ebd525b4b09177df56c94040eb49ad11791661e
Alibek Suleimenov <alibek443@gmail.com>
2019-07-06 17:29:18 +0600

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig : {
-    apiKey: "AIzaSyCf0eP6h0M_mEIyCfrwr1QC9GT9cYA94lA",
-    authDomain: "angularfirebase-app-1290c.firebaseapp.com",
-    databaseURL: "https://angularfirebase-app-1290c.firebaseio.com",
-    projectId: "angularfirebase-app-1290c",
+    apiKey: "#",
+    authDomain: "#",
+    databaseURL: "#",
+    projectId: "#",
     storageBucket: "",
-    messagingSenderId: "617301023385",
-    appId: "1:617301023385:web:18147e59f4efb8b8"
+    messagingSenderId: "#",
+    appId: "#"
   }
 };
 
